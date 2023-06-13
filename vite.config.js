95b95e29a9aaf5dc15420ad09f26110c8041cd61
iamroms <54948947+rcenamsi@users.noreply.github.com>
2023-06-14 02:16:42 +0800

    - update on config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -11,4 +12,7 @@ export default defineConfig({
         }),
         react(),
     ],
+    resolve: {
+        ziggy: path.resolve('./vendor/tightenco/ziggy/dist/index.m')
+    }
 });
